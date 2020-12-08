355ea40d49bd97544a434d62d665b0394064a36e
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2020-12-07 19:20:51 -0800

    add super-basic cloud-ish animation

---
+++ src/Clouds.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Animate } from 'react-move';
+
+const colors = [
+    "#333", "#444", "#777", "#999", "#AAA"
+]
+
+const useTick = (delay, initialIndex) => {
+    const [tick, setTick] = useState(initialIndex ? initialIndex : 0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!document.hidden) {
+                setTick((tick) => tick + 1);
+            }
+        }, delay);
+        return () => clearInterval(interval);
+    }, [delay]);
+    return tick;
+}
+
+const Bar = ({ x, y, width, height, fill, tickTiming }) => {
+    return (
+        <Animate start={{ height }} enter={{ height: [height], timing: tickTiming }} update={{ height: [height], timing: tickTiming }}>
+            {(state) => <rect x={x} y={y} width={width} height={height} fill={fill}></rect>}
+        </Animate>
+    );
+}
+
+const Clouds = () => {
+    const delay = 41;
+    const index = useTick(delay, 0);
+    console.log("index", index);
+    let calculatedHeight = 20 * (Math.sin(index / 24) + 1) + 10;
+    console.log(calculatedHeight);
+
+    return (
+        <div style={{ width: 300, position: "absolute", top: 180, left: 800 }}>
+            <svg version="1.1" viewBox="0 0 180 150">
+                <g id="root">
+                    <g>
+                        {[36, 26, 9, 9, 26].map((d, i) => <Bar tickTiming={index} x={60 * (1 - i / 4) + 24 * i + 50} y={(75 + i * 4)} width={24 / (1.0 + (calculatedHeight / 60))} height={calculatedHeight} fill={`${colors[i]}${7}`} />)}
+                    </g>
+                </g>
+            </svg>
+        </div>
+    )
+};
+
+export default Clouds;
