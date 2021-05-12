c1e4dcbffd88e010877009ad3f6624c7cf9dccf7
brock <brocksmedley@gmail.com>
2021-05-12 00:28:16 -0700

    make clouds more sexy

--- src/Clouds.jsx
+++ src/Clouds.jsx
@@ -30,14 +30,14 @@ const Clouds = () => {
     const delay = 41;
     const maxIndex = 9001;
     const index = useTick(delay, 0) % maxIndex;
-    let calculatedHeight = 20 * (Math.sin(index / 24) + 1) + 10;
+    let calculatedHeight = 20 * (Math.sin(index / 24) + 1) + 1;
 
     return (
-        <div style={{ width: 300, position: "absolute", top: 168, left: 820 }}>
+        <div style={{ width: 300, position: "absolute", top: 155, left: 820 }}>
             <svg version="1.1" viewBox="0 0 180 150">
                 <g id="root">
                     <g>
-                        {[36, 26, 9, 9, 26].map((d, i) => <Bar key={i} tickTiming={index} x={60 * (1 - i / 4) + 24 * i + 50} y={(75 + i * 4) + (i * (calculatedHeight / 5)) * Math.cos(i * 10 + calculatedHeight / 4)} width={16} height={calculatedHeight / 1.5 + 10} fill={`${colors[i]}${7}`} />)}
+                        {[36, 26, 9, 9, 26].map((d, i) => <Bar key={i} tickTiming={index} x={60 * (1 - i / 4) + 24 * i + 50} y={90 + i * 4 + (i * (calculatedHeight / 5)) * Math.cos(i * 10 + calculatedHeight / 4)} width={16} height={calculatedHeight / 1.5 + 10} fill={`${colors[i]}${7}`} />)}
                     </g>
                 </g>
             </svg>
